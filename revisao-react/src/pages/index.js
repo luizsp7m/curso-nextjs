9ad76d193da472ac9a1da2071067343159ca1c0b
Luiz <luizoliveira2808@gmail.com>
2021-10-02 22:12:23 -0300

    Reestruturação do projeto

---
+++ revisao-react/src/pages/index.js
@@ -0,0 +1,23 @@
+import style from '../../styles/Home.module.css';
+
+import { Fragment } from 'react';
+
+import Head from 'next/head';
+
+import Header from "../components/Header";
+
+export default function Home() {
+  return (
+    <Fragment>
+      <Head>
+        <title>Homepage</title>
+      </Head>
+
+      <Header />
+
+      <div className={style.container}>
+        Reestruturação do repositório
+      </div>
+    </Fragment>
+  );
+}
